793106c411c14f5497a81b929466d63a8858a869
vladick <mamrokhavlad@gmail.com>
2025-11-27 12:27:55 +0200

    keys_comparison definition

--- src/node.cpp
+++ src/node.cpp
@@ -18,12 +18,17 @@ bool Node::is_leaf()
     return children.empty();
 };
 
-Node* Node::keys_comparison(unsigned key, std::vector<unsigned>&)
+Node* Node::keys_comparison(unsigned key)
 {
-
+    unsigned i;
+    for( i = 0; i < keys.size(); i++)
+        if(key < keys[i]) break;
+    return children[i];
 };
 
 bool Node::is_full()
 {
     return children.size() >= capacity || keys.size() >= capacity - 1;
 };
+
+
