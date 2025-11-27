401f8a849f72fed5a9c2f8ec3d5577b7ea8a04af
vladick <mamrokhavlad@gmail.com>
2025-11-27 12:00:05 +0200

    updated tree constructor to set root to null. insert method def not complete(check for root == null)

--- src/tree.cpp
+++ src/tree.cpp
@@ -1,11 +1,25 @@
 #include "tree.hpp"
 
 
-Tree::Tree(unsigned order): order(order) {};
+Tree::Tree(unsigned order): order(order) {
+    root = nullptr;
+};
 
 void Tree::remove(unsigned key)
 {};
 
-void Tree::insert(unsigned key){};
+void Tree::insert(unsigned key)
+{
+    if(root == nullptr)
+    {
+        root = new Node(order);
+        root->insert(key);
+    }else{
+        Node* leaf = root->get_leaf(key);
+        
+    
+    };
+
+};
 
 void Tree::search(unsigned key){};
