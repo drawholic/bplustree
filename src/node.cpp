2c6490fd9bb2d018d1ea3a66eaeb8ecd609c9517
vladick <mamrokhavlad@gmail.com>
2025-11-27 11:51:53 +0200

    Node(unsigned) and is_leaf definitions

--- src/node.cpp
+++ src/node.cpp
@@ -5,3 +5,15 @@ Node::Node()
 {
 
 };
+
+
+Node::Node(unsigned order): capacity(order)
+{
+    children.reserve(capacity);
+    keys.reserve(capacity);
+};
+
+bool Node::is_leaf()
+{
+    return children.empty();
+};
