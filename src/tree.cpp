ef1f8bf0673176f26daff7d77fcec8f884dff6f4
vladick <mamrokhavlad@gmail.com>
2025-11-27 11:45:25 +0200

    null methods defined

--- src/tree.cpp
+++ src/tree.cpp
@@ -1,7 +1,11 @@
 #include "tree.hpp"
 
 
-Tree::Tree()
-{
+Tree::Tree(unsigned order): order(order) {};
 
-};
+void Tree::remove(unsigned key)
+{};
+
+void Tree::insert(unsigned key){};
+
+void Tree::search(unsigned key){};
