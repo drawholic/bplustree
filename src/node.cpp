e00b72eff9b38845396d449e622042cf2cdf5bb6
vladick <mamrokhavlad@gmail.com>
2025-11-27 11:57:53 +0200

    changed keys reservation capacity to order + 1

--- src/node.cpp
+++ src/node.cpp
@@ -10,7 +10,7 @@ Node::Node()
 Node::Node(unsigned order): capacity(order)
 {
     children.reserve(capacity);
-    keys.reserve(capacity);
+    keys.reserve(capacity+1);
 };
 
 bool Node::is_leaf()
