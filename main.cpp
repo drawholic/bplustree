500ca7f0e51492c2e08ae46c4d0b7b404c245763
vladick <mamrokhavlad@gmail.com>
2025-11-27 12:14:26 +0200

    tree is initialized with order cli argument

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "tree.hpp"
-
+#include <cstdlib>
 
 int main(int argc, char const *argv[])
 {
-    Tree t;
+    Tree t(
+        std::atoi(argv[1])
+        );
+
+        
     return 0;
 }
