b9be05a02dbb605e38afc7d0f4a6cce17c884249
vladick <mamrokhavlad@gmail.com>
2025-11-27 11:50:23 +0200

    node class declaration update. is_leaf method and node(unsigned) constructor. children, keys and capacity fields added

--- include/node.hpp
+++ include/node.hpp
@@ -1,13 +1,24 @@
 #ifndef NODE_HPP
 #define NODE_HPP
 
+#include <vector>
 
 class Node
 {
 
+    std::vector<Node*> children;
+    std::vector<unsigned> keys;
+
+    unsigned capacity;
 
 public:
     Node();
+    Node(unsigned);
+
+
+
+    bool is_leaf();
+
 };
 
 
