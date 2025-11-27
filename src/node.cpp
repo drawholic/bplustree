0a40829bfdc3ce646eccaca7baa5b3d407b3055f
vladick <mamrokhavlad@gmail.com>
2025-11-27 12:49:35 +0200

    split definition

--- src/node.cpp
+++ src/node.cpp
@@ -31,4 +31,27 @@ bool Node::is_full()
     return children.size() >= capacity || keys.size() >= capacity - 1;
 };
 
+void Node::split()
+{
+    unsigned pivot = capacity / 2;
+    Node* pivot_node = children[pivot];
+    unsigned pivot_key = keys[pivot];
+
+    Node* left = new Node(capacity);
+    Node* right = new Node(capacity);
+    
+    left->copy_children(children.begin(), children.begin()+pivot);
+    left->copy_keys(keys.begin(), keys.begin()+pivot);
+
+    right->copy_children(children.begin()+ pivot + 1, children.end());
+    right->copy_keys(keys.begin()+pivot+1, keys.end());
+
+    children.clear();
+    keys.clear();    
+
+    children.push_back(pivot_node);
+    keys.push_back(pivot_key);
+
+};
+
 
