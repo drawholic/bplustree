a14c4410262567417ac2b48b52771a1d464faebf
vladick <mamrokhavlad@gmail.com>
2025-11-27 12:18:23 +0200

    node constructor fixed number of reserve for keys. is_full definition

--- src/node.cpp
+++ src/node.cpp
@@ -10,10 +10,20 @@ Node::Node()
 Node::Node(unsigned order): capacity(order)
 {
     children.reserve(capacity);
-    keys.reserve(capacity+1);
+    keys.reserve(capacity-1);
 };
 
 bool Node::is_leaf()
 {
     return children.empty();
+};
+
+Node* Node::keys_comparison(unsigned key, std::vector<unsigned>&)
+{
+
+};
+
+bool Node::is_full()
+{
+    return children.size() >= capacity || keys.size() >= capacity - 1;
 };
